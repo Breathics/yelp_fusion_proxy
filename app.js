56e3d5d7abf381b576bace468c866cba299e02d5
Andy Ong <aong93@gmail.com>
2017-10-10 19:53:47 -0700

    nevermind needed cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const yelp = require('yelp-fusion');
 const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
