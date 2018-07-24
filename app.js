51f5319fc63b368c9ef20734e559cfc24fd84ec2
Andy Ong <aong93@gmail.com>
2018-07-24 11:19:47 -0700

    made changes to endpoints to reflect change in yelp api. use of access token is now deprecated

--- app.js
+++ app.js
@@ -17,21 +17,19 @@ app.get('/', (req, res) => {
   res.send("Luigi");
 })
 
-// Search should be using Access Token
+// Search should be using API key
 // Then some search parameters
 app.post('/search', (req, res) => {
-  if (req.body.access_token == undefined){
-    res.json({"Success": false, errors: "Please enter a valid access token"});
+  if (req.body.api_key == undefined){
+    res.json({"Success": false, errors: "Please enter a valid API key"});
   }
-  const client = yelp.client(req.body.access_token);    
+  const client = yelp.client(req.body.api_key);    
 
   var params = {
     term: req.body.term,
     location: req.body.location
   }
   client.search(params).then(response => {
-    // console.log(response.jsonBody.businesses[0].name);
-    // res.json(response.jsonBody.businesses[0].name);
     res.json(response.jsonBody);
   }).catch(e => {
     var errors = [];
@@ -39,15 +37,41 @@ app.post('/search', (req, res) => {
     if (req.body.location == undefined) errors.push("Please enter a valid location");
     res.json({"Success": false, errors});
   });
-})
+});
 
+
+app.post('/businesses', (req, res) => {
+  if (req.body.api_key == undefined){
+    res.json({"Success": false, errors: "Please enter a valid API key"});
+  }
+  const client = yelp.client(req.body.api_key);
+  var keys = Object.keys(req.body);
+  var params = {};
+  for(var key in req.body) {
+    params[key] = req.body[key];
+  }
+
+  client.search(params).then(response => {
+    res.json(response.jsonBody);
+  }).catch(e => {
+    var errors = [];
+    var keys = Object.keys(req.body);
+
+    for (var key in req.body) {
+      if (req.body[key] === '') {
+        errors.push(`Please enter a valid ${key}`);
+      }
+    }
+    res.json({"Success": false, errors});
+  });
+});
+
+// Excluding transactions
 // app.post('/transaction', (req, res) => {
-//   if (req.body.access_token == undefined){
-//     res.json({"Success": false, errors: "Please enter a valid access token"});
+//   if (req.body.api_key == undefined){
+//     res.json({"Success": false, errors: "Please enter a valid API key"});
 //   }
-//   const client = yelp.client(req.body.access_token);  
-  
-//   console.log('this is the req.body', req.body);
+//   const client = yelp.client(req.body.api_key);  
 
 //   client.transactionSearch('delivery', {
 //     location: 'san diego'
@@ -62,10 +86,10 @@ app.post('/search', (req, res) => {
 // })
 
 app.post('/reviews', (req, res) => {
-  if (req.body.access_token == undefined){
-    res.json({"Success": false, errors: "Please enter a valid access token"});
+  if (req.body.api_key == undefined){
+    res.json({"Success": false, errors: "Please enter a valid API key"});
   }
-  const client = yelp.client(req.body.access_token);    
+  const client = yelp.client(req.body.api_key);    
   
   client.reviews(req.body.business_id).then(response => {
     res.json(response.jsonBody);
